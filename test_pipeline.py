1f1bf600b6e34de7ab35aa415b21a43079726bb3
Bradley Baker <bbradt@gmail.com>
2019-03-20 23:31:22 -0600

    test pipeline

--- test_pipeline.py
+++ test_pipeline.py
@@ -1,20 +1,97 @@
 import json
-import decentralized-pca
-import coinstac_globalpca
-import coinstac_gica
-import coinstac_backreconstruction
-import coinstac_ddfnc_preproc
-import coinstac_dkmeans_ms
 
-PYTHON_CMD = "python %s %s"
+PYTHON_CMD = "python %s/%s %s"
 
 PIPELINE = [
-    decentralized-pca
+    'decentralized_row_means',
+    #    decentralized_pca
     #    coinstac_gica,
     #    coinstac_backreconstruction,
     #    coinstac_ddfnc_preproc,
     #    coinstac_dkmeans_ms
 ]
 
+INPUTS = [
+    [
+        {
+            "data": {
+                "value": [
+                    [
+                        "local0.sub0.data.nifti",
+                    ],
+                    [
+                        "nifti"
+                    ]
+                ]
+            }
+        },
+        {
+            "data": {
+                "value": [
+                    [
+                        "local1.sub1.data.nifti",
+                    ],
+                    [
+                        "nifti"
+                    ]
+                ]
+            }
+        }
+    ],
+    [{
+        "input": {
+            "data": [
+                [
+                    "local0.sub0.data.nifti",
+                ],
+                [
+                    "nifti"
+                ]
+            ],
+            "num_PC_global": 20,
+            "axis": -1,
+            "mean_values": [
+                [
+                    "row_mean_global.npz"
+                ],
+                [
+                    "npzfile"
+                ]
+            ],
+            "subject_level_PCA": True,
+            "subject_level_num_PC": 120
+        },
+        "cache": {}
+    },
+        {
+        "input": {
+            "data": [
+                [
+                    "local1.sub1.data.nifti",
+                ],
+                [
+                    "nifti"
+                ]
+            ],
+            "num_PC_global": 20,
+            "axis": -1,
+            "mean_values": [
+                [
+                    "row_mean_global.npz"
+                ],
+                [
+                    "npzfile"
+                ]
+            ],
+            "subject_level_PCA": True,
+            "subject_level_num_PC": 120
+        },
+        "cache": {}
+    }
+    ]
+]
 
-for stage in PIPELINE:
+
+for stage, inputs in zip(PIPELINE, INPUTS):
+    python_cmd = PYTHON_CMD % (stage, 'local.py', json.dumps(inputs))
+    print(python_cmd)
